0d024f4e459b8de916648a45001b0b05ebd81758
King <nguyenquocvuong12321@gmail.com>
2026-01-18 11:34:53 +0700

    update readme

--- demo/src/main/java/com/oliolishop/oliolishop/constant/ApiPath.java
+++
@@ -1,9 +0,0 @@
-package com.oliolishop.oliolishop.constant;
-
-import java.security.PublicKey;
-
-public class ApiPath {
-    public static final String BASE = "/api";
-    public static final String BY_ID = "/{id}";
-
-}
